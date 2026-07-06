a2d6cb584ed1cd4ac624cd362aa1d5a1655af014
moslem <moslemamiri82@gmail.com>
2026-07-06 18:14:24 +0330

    add About Page

--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,9 @@ from django.views import generic
 class HomeView(generic.TemplateView):
     template_name = "pages/home.html"
 
+
+class AboutView(generic.TemplateView):
+    template_name = "pages/about.html"
+
+class ContactView(generic.TemplateView):
+    template_name = "pages/contact.html"
