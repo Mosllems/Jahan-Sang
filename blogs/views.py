2ce665ccfbedc052b574b7cfa35fad186136d863
moslem <moslemamiri82@gmail.com>
2026-07-17 19:25:30 +0330

    Add BlogDetailView

--- blogs/views.py
+++ blogs/views.py
@@ -33,3 +33,10 @@ class BlogDetailView(generic.DetailView):
     template_name = "blogs/blog_detail.html"
     context_object_name = "blog"
 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        context['recent_blogs'] = Blog.objects.select_related('category', 'author').all()[:3]
+        return context
+
